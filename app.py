e20febad6cb04e3bb9b0d67ca3b8baefc7272b37
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2025-07-05 11:18:27 +0200

    feat: added templates for other sites and improved styling

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def main():
     return render_template('home.html')
 
+@app.route('/tools', methods=['GET', 'POST'])
+def tools():
+    return render_template('tools.html')
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     with app.app_context():
         app.run(debug=True, port=5000)
