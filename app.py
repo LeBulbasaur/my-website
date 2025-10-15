82eabc9ab2b0c69b95e3a6afdc82476d9753d700
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2025-10-15 18:40:13 +0200

    feat: remove not used parts

--- app.py
+++ app.py
@@ -11,15 +11,6 @@ def main():
 def tools():
     return render_template('tools.html')
 
-@app.route('/tools/trambus', methods=['GET', 'POST'])
-def trambus():
-    schedule = get_trambus_schedule('1234567890')
-    return render_template('tools/trambus.html', schedule=schedule)
-
-@app.route('/about', methods=['GET', 'POST'])
-def about():
-    return render_template('about.html')
-
 if __name__ == '__main__':
     with app.app_context():
         app.run(debug=True, port=5000)
