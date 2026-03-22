cd84dea60346fcac9e0e464bb9d4611f4429fb63
Antoni Zaremba <59770761+LeBulbasaur@users.noreply.github.com>
2026-03-23 00:20:34 +0100

    feat: added article webiste

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from os import path
 from articles import load_articles
+import json
+from flask import abort
 
 app = Flask(__name__)
 
@@ -15,6 +17,17 @@ def blog():
     articles_dir = path.join(app.root_path, "articles")
     articles = load_articles(articles_dir)
     return render_template('blog.html', articles=articles)
+
+
+@app.route('/blog/<slug>', methods=['GET'])
+def article(slug):
+    """Show raw/prettified JSON for a single article identified by slug."""
+    articles_dir = path.join(app.root_path, "articles")
+    articles = load_articles(articles_dir)
+    for a in articles:
+        if a.get('slug') == slug:
+            return render_template('article.html', article=a)
+    abort(404)
 
 if __name__ == '__main__':
     with app.app_context():
